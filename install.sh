9ae720d22bae48db4543f09b897458e6d6e35124
Jishith <jishithmp534@gmail.com>
2026-06-10 12:38:48 +0530

    update installer and fixes

--- install.sh
+++ install.sh
@@ -56,7 +56,7 @@ echo "Building runtime ..."
 
 llc -filetype=obj src/zen_stdlib/constants.ll -o src/zen_stdlib/constants.o
 
-llc -filetype=obj src/zen_stdlib/zen_stdlib_opt.ll -o src/zen_stdlib/stdlib_opt.o
+llc -filetype=obj src/zen_stdlib/zen_stdlib_opt.ll -o src/zen_stdlib/zen_stdlib_opt.o
 
 
 clang -c src/codegen/runtime/runtime.c -o src/codegen/runtime/runtime.o
