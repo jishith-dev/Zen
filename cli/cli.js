98067f31d744bc48ec1ea80733f088e3da40c05e
Jishith MP <jishithmp534@gmail.com>
2026-08-27 14:36:10 +0530

    updated version number in cli

--- cli/cli.js
+++ cli/cli.js
@@ -69,7 +69,7 @@ const COMPILE_COMMANDS = new Set([
 
 function help() {
   console.log(`
-Zen Programming Language v2.0.0
+Zen Programming Language v2.1.0
 
 hint: ? (optional)
 
@@ -142,7 +142,7 @@ export class CLI {
     }
 
     if (command === "--version" || command === "-v" || command === "version") {
-      console.log("Zen v2.0.0 (latest)");
+      console.log("Zen v2.1.0 (latest)");
       process.exit(0);
     }
 
